f2b58fafd002cf2b1386a6faa2b474c18298f8fb
Jason Hamilton <musicman3569@gmail.com>
2025-08-07 20:50:25 -0600

    Add services for controllers and CORS + env file

--- StarWarsInfo/Program.cs
+++ StarWarsInfo/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClientApp", policy =>
+    {
+        policy.WithOrigins(Environment.GetEnvironmentVariable("CORS_ORIGIN") ?? "*")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
