38dcef4e50d7f28626273c22c954be4b5e1e489e
Jason Hamilton <musicman3569@gmail.com>
2025-08-16 11:58:43 -0600

    Remove all Kendo references

--- StarWarsClient/src/App.tsx
+++ StarWarsClient/src/App.tsx
@@ -1,11 +1,9 @@
-import StarshipsTable from "./components/StarshipsTable"
-import '@progress/kendo-theme-material/dist/material-main-dark.css'
 import './App.css'
 
 function App() {
   return (
     <>
-      <StarshipsTable />
+      
     </>
   )
 }
