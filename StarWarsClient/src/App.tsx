cd013846f9f95c7e5848797f7dcc6b2e88afdb98
Jason Hamilton <musicman3569@gmail.com>
2025-08-16 12:10:42 -0600

    Install PrimeReact npm package

--- StarWarsClient/src/App.tsx
+++ StarWarsClient/src/App.tsx
@@ -1,10 +1,11 @@
+import { PrimeReactProvider } from 'primereact/api';
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <PrimeReactProvider>
+      <h1>Starships</h1>
+    </PrimeReactProvider>
   )
 }
 
