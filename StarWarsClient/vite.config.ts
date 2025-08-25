d1e3c0d42f99fb762a818ef5fee255a672aea44a
Jason Hamilton <musicman3569@gmail.com>
2025-08-24 18:54:38 -0600

    All vite preview to get port from .env as well

--- StarWarsClient/vite.config.ts
+++ StarWarsClient/vite.config.ts
@@ -18,5 +18,13 @@ export default defineConfig({
       host: process.env.VITE_HOSTNAME_CLIENT,
       port: Number(process.env.VITE_CLIENT_HTTPS_PORT) || 443
     }
-  }
+  },
+    preview: {
+        host: '0.0.0.0',
+        port: Number(process.env.VITE_CLIENT_HTTPS_PORT) || 443,
+        https: {
+            cert: fs.readFileSync(path.resolve('/SSL/' + process.env.VITE_HOSTNAME_CLIENT + '.crt')),
+            key: fs.readFileSync(path.resolve('/SSL/' + process.env.VITE_HOSTNAME_CLIENT + '.key'))
+        }
+    }
 })
